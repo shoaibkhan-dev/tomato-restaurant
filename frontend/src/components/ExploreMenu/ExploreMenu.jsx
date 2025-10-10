5aeeae9d92802bfe75ac2360a4e27b9e986e450a
Shoaib khan <shoaibmangal@yahoo.com>
2025-10-10 22:52:09 +0500

    exploremenu page function

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -22,3 +22,14 @@ const ExploreMenu = ({category,setCategory}) => {
 }
 
 export default ExploreMenu;
+const ExploreMenu = ({category,setCategory}) =>{
+  return(
+    <div className="explore-menu">
+      <p className='explore-menu'>
+        {menu_list.map((item,index)=>{
+          
+        })}
+      </p>
+    </div>
+  )
+}
